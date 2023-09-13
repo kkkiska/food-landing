c89dceda16b152d721a25f37c1edc0ad8fc4fabb
Pavel <zenko_pavel@mail.ru>
2023-09-13 13:12:39 +0300

    Js Timer

--- js/script.js
+++ js/script.js
@@ -35,4 +35,58 @@ document.addEventListener('DOMContentLoaded', () => {
             })
         }
     })
+
+    // Timer
+
+    const deadline = '2024-05-20'
+    
+    function getTimeRemaining(endtime) {
+        const t = Date.parse(endtime) - Date.parse(new Date());
+        const days = Math.floor(t / (1000 * 60 * 60 * 24))
+        const hours = Math.floor ((t / (1000 * 60 * 60)) % 24)
+        const minutes = Math.floor((t / (1000 * 60)) % 60)
+        const seconds = Math.floor ((t / 1000) % 60)
+
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        }
+    }
+
+    function addZero(num) {
+        if (num >= 0 && num < 10) {
+            return `0${num}`
+        } else {
+            return num
+        }
+    }
+
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector);
+        const days = timer.querySelector('#days')
+        const hours = timer.querySelector('#hours')
+        const minutes = timer.querySelector('#minutes')
+        const seconds = timer.querySelector('#seconds')
+        const timeInterval = setInterval(updateClock, 1000)
+        
+        updateClock()
+
+        function updateClock() {
+            const t = getTimeRemaining(endtime)
+    
+            days.innerHTML = addZero(t.days)
+            hours.innerHTML = addZero(t.hours)
+            minutes.innerHTML = addZero(t.minutes)
+            seconds.innerHTML = addZero(t.seconds)
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval)
+            }
+        }
+    }
+
+    setClock('.timer', deadline)
 })
